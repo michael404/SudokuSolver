a8189a62a275e186b9eeb8f2d9d7a6eda9af14a5
Michael <michael404@gmail.com>
2017-08-31 20:19:10 +0200

    Initial Commit

---
+++ SudokuSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SudokuSolver
+//
+//  Created by Michael Holmgren on 2017-08-31.
+//  Copyright © 2017 Michael Holmgren. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
