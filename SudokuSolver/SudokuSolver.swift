9084afacb804d4d7cd753f9fc7102dda0710e353
Michael <michael404@gmail.com>
2018-01-04 07:16:55 +0100

    Changed _solve() to be an local function closing over the local state instead of passing inout arguments

--- SudokuSolver/SudokuSolver.swift
+++ SudokuSolver/SudokuSolver.swift
@@ -9,36 +9,38 @@ struct SudokuSolver {
     }
     
     func solve() throws -> SudokuBoard {
+        
         var board = self.initialBoard
         let indiciesIterator = board.indices.filter { board[$0] == nil }.makeIterator()
         var validator = SudokuValidator(board)
-        guard _solve(board: &board, indiciesIterator: indiciesIterator, validator: &validator) else {
+        
+        func _solve(_ indiciesIterator: Array<Int>.Iterator) -> Bool {
+            var indiciesIterator = indiciesIterator
+            
+            // Check if we reached the end
+            guard let index = indiciesIterator.next() else { return true }
+            
+            for cell in (1...9) {
+                board[index] = SudokuCell(cell)
+                let coordinate = SudokuCoordinate(index)
+                if validator.validate(cell, at: coordinate) {
+                    validator.set(cell, to: true, at: coordinate)
+                    if _solve(indiciesIterator) {
+                        return true
+                    } else {
+                        validator.set(cell, to: false, at: coordinate)
+                    }
+                }
+            }
+            // Tried all possible values for this cell without finding a valid one, so returning false
+            board[index] = nil
+            return false
+        }
+        
+        guard _solve(indiciesIterator) else {
             throw SudokuSolverError.unsolvable
         }
         return board
-    }
-    
-    private func _solve(board: inout SudokuBoard, indiciesIterator: Array<Int>.Iterator, validator: inout SudokuValidator) -> Bool {
-        var indiciesIterator = indiciesIterator
-        
-        // Check if we reached the end
-        guard let index = indiciesIterator.next() else { return true }
-        
-        for cell in (1...9) {
-            board[index] = SudokuCell(cell)
-            let coordinate = SudokuCoordinate(index)
-            if validator.validate(cell, at: coordinate) {
-                validator.set(cell, to: true, at: coordinate)
-                if _solve(board: &board, indiciesIterator: indiciesIterator, validator: &validator) {
-                    return true
-                } else {
-                    validator.set(cell, to: false, at: coordinate)
-                }
-            }
-        }
-        // Tried all possible values for this cell without finding a valid one, so returning false
-        board[index] = nil
-        return false
     }
     
 }
