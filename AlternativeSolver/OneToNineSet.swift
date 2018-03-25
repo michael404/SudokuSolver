913e35c4450f3161b6a8f1356906acce41b467ca
Michael <michael404@gmail.com>
2018-03-25 22:08:19 +0200

    Improved OneToNineSet

--- AlternativeSolver/OneToNineSet.swift
+++ AlternativeSolver/OneToNineSet.swift
@@ -2,22 +2,18 @@ struct OneToNineSet: Sequence {
     
     private var _storage: UInt16
     
-    private var _count: UInt8
-    
     //TODO: Evaluate if this cache is necessary after other performance optimizations
     /// The only value if there is only one value. 0 otherwise
     fileprivate var _onlyValue: UInt8
     
     init(allTrue: ()) {
         self._storage = 0b1111111110
-        self._count = 9
         self._onlyValue = 0
     }
     
     init(_ value: Int) {
         assert((1...9).contains(value))
         self._storage = 1 << value ^ 0
-        self._count = 1
         self._onlyValue = UInt8(truncatingIfNeeded: value)
     }
     
@@ -32,8 +28,7 @@ struct OneToNineSet: Sequence {
         let oldValue = ((_storage >> value) & 1) == 1
         if oldValue {
             _storage = 1 << value ^ _storage
-            _count = _count &- 1
-            if _count == 1 {
+            if hasSingeValue {
                 for index in 1...9 where contains(index) {
                     _onlyValue = UInt8(truncatingIfNeeded: index)
                 }
@@ -44,12 +39,15 @@ struct OneToNineSet: Sequence {
     }
     
     var count: Int {
-        return Int(truncatingIfNeeded: _count)
+        // http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSet64
+        return (Int(truncatingIfNeeded: _storage) * 0x200040008001 & 0x111111111111111) % 0xf
     }
     
     var hasSingeValue: Bool {
-        return _count == 1
+        return count == 1
     }
+    
+
     
     var onlyValue: Int? {
         guard _onlyValue != 0 else { return nil }
@@ -81,8 +79,8 @@ struct OneToNineIterator: IteratorProtocol {
             defer { index = index &+ 1 }
             if base.contains(index) { return index }
         } while index < 10
-        
         return nil
+        
     }
     
 }
