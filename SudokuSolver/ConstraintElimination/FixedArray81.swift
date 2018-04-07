16bbda02dbd96d6afe7b5e110055f105ca0b181e
Michael <michael404@gmail.com>
2018-04-07 15:49:43 +0200

    Made the FixedArray81-implementation more pretty

--- SudokuSolver/ConstraintElimination/FixedArray81.swift
+++ SudokuSolver/ConstraintElimination/FixedArray81.swift
@@ -2,27 +2,27 @@ struct FixedArray81<T> {
 
     // 81 elements
     var storage: (
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T, T, T, T, T, T, T, T, T, T,
-        T)
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T,
+        T, T, T, T, T, T, T, T, T)
     
     init(repeating value: T) {
         self.storage = (
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value, value, value, value, value, value, value, value, value, value,
-            value)
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value,
+            value, value, value, value, value, value, value, value, value)
     }
     
 }
@@ -36,32 +36,24 @@ extension FixedArray81 : RandomAccessCollection, MutableCollection {
     subscript(i: Int) -> T {
         @inline(__always)
         get {
+            //TODO: If SE-0205 passes - see if we can eliminate the copy here
             var copy = storage
-            let result: T = withUnsafeBytes(of: &copy) {
-                (rawPtr : UnsafeRawBufferPointer) -> T in
-                let stride = MemoryLayout<T>.stride
-                assert(rawPtr.count == 81*stride, "layout mismatch?")
-                let bufPtr = UnsafeBufferPointer(
-                    start: rawPtr.baseAddress!.assumingMemoryBound(to: T.self),
-                    count: 81)
-                return bufPtr[i]
+            return withUnsafeBytes(of: &copy) { (rawPtr : UnsafeRawBufferPointer) -> T in
+                let buffer = UnsafeBufferPointer(start: rawPtr.baseAddress!.assumingMemoryBound(to: T.self), count: 81)
+                return buffer[i]
             }
-            return result
         }
         @inline(__always)
         set {
-            withUnsafeMutableBytes(of: &storage) { rawBuffer in
-                assert(rawBuffer.count == 81*MemoryLayout<T>.stride, "layout mismatch?")
-                let buffer = UnsafeMutableBufferPointer<Element>(
-                    start: rawBuffer.baseAddress._unsafelyUnwrappedUnchecked.assumingMemoryBound(to: Element.self),
-                    count: 81)
+            withUnsafeMutableBytes(of: &storage) { rawPtr in
+                let buffer = UnsafeMutableBufferPointer<Element>(start: rawPtr.baseAddress!.assumingMemoryBound(to: Element.self), count: 81)
                 buffer[i] = newValue
             }
         }
     }
     
-    func index(after i: Int) -> Int { return i+1 }
+    func index(after i: Int) -> Int { return i + 1 }
     
-    func index(before i: Int) -> Int { return i-1 }
+    func index(before i: Int) -> Int { return i - 1 }
 }
 
