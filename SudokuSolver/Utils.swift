c0ba748405bedf1c6a99580c2db16337dbb89e37
Michael <michael404@gmail.com>
2018-08-16 21:43:37 +0200

    Remove code that was commented out

--- SudokuSolver/Utils.swift
+++ SudokuSolver/Utils.swift
@@ -3,23 +3,6 @@ enum SudokuSolverError: Error {
     case tooManySolutions
 }
 
-//struct SudokuCoordinate {
-//
-//    let index: Int
-//    let row: Int
-//    let column: Int
-//    let block: Int
-//
-//    init(_ index: Int) {
-//        self.index = index
-//        self.row = index / 9
-//        self.column = index % 9
-//        self.block = (self.row / 3) * 3 + (self.column / 3)
-//    }
-//
-//}
-
-
 typealias RNG = RandomNumberGenerator
 
 // Adapted from https://github.com/mattgallagher/CwlUtils/blob/master/Sources/CwlUtils/CwlRandom.swift
