fbb29fe75c1ca9bcc30fa0f87fec6e543803f66a
Michael <michael404@gmail.com>
2019-11-15 19:51:42 +0100

    Modernized the setup, including moving perf tests to separate target

---
+++ SudokuPerfTests/SudokuPerfTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+
+class SudokuPerfTests: XCTestCase {
+    
+    func testPerfSuite() {
+        var solutions = [SudokuBoard]()
+        self.measure {
+            for board in TestData.PerfTestSuite.boards {
+                let solvedBoard = try! board.findFirstSolution()
+                solutions.append(solvedBoard)
+            }
+        }
+        for (solvedBoard, expectedSolution) in zip(solutions, TestData.PerfTestSuite.solutions) {
+            XCTAssertEqual(solvedBoard, expectedSolution)
+        }
+    }
+    
+    func testPerfRandomFullyFilledBoard() {
+        var board = SudokuBoard.empty
+        self.measure {
+            var rng = Xoroshiro(seed: (42, 42))
+            for _ in 0..<10 {
+                board = SudokuBoard.randomFullyFilledBoard(rng: &rng)
+            }
+        }
+        XCTAssertEqual(board, SudokuBoard("983624571421587369567931482854196237239758614716243958145862793672319845398475126"))
+        XCTAssertTrue(board.isValid)
+        XCTAssertTrue(board.isFullyFilled)
+        XCTAssertEqual(board.clues, 81)
+    }
+    
+    func testPerfRandomStartingBoard() {
+        var board = SudokuBoard.empty
+        self.measure {
+            var rng = Xoroshiro()
+            for _ in 0..<10 {
+                board = SudokuBoard.randomStartingBoard(rng: &rng)
+            }
+        }
+        XCTAssertEqual(board.numberOfSolutions(), .one)
+        XCTAssertTrue(board.isValid)
+        XCTAssertFalse(board.isFullyFilled)
+        XCTAssertTrue((17...40).contains(board.clues))
+    }
+}
+
+
