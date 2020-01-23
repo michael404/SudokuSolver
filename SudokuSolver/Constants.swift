291d66d3142a0b8eef2c2fef898ec5812b2dfcda
Michael <michael404@gmail.com>
2020-01-23 20:53:05 +0100

    Simplified the generics into a simple struct

--- SudokuSolver/Constants.swift
+++ SudokuSolver/Constants.swift
@@ -1,27 +1,15 @@
-let Constants = ConstantsStorage<SudokuSize9>()
-let Constants16 = ConstantsStorage<SudokuSize16>()
+let Constants = ConstantsStorage(size: SudokuSize(sideOfBox: 3))
+let Constants16 = ConstantsStorage(size: SudokuSize(sideOfBox: 4))
 
-protocol SudokuSize {
-    static var sideOfBox: Int { get }
+struct SudokuSize {
+    let sideOfBox: Int
+    var possibilities: Int { sideOfBox * sideOfBox }
+    var allPossibilities: Range<Int> { 0..<possibilities }
+    var cells: Int { possibilities * possibilities }
+    var allCells: Range<Int> { 0..<cells }
 }
 
-extension SudokuSize {
-    static var possibilities: Int { sideOfBox * sideOfBox }
-    static var allPossibilities: Range<Int> { 0..<possibilities }
-    static var cells: Int { possibilities * possibilities }
-    static var allCells: Range<Int> { 0..<cells }
-}
-
-enum SudokuSize9: SudokuSize {
-    static let sideOfBox: Int = 3
-}
-
-enum SudokuSize16: SudokuSize {
-    static let sideOfBox: Int = 4
-}
-
-
-struct ConstantsStorage<Size: SudokuSize> {
+struct ConstantsStorage {
     
     /// The 39  indicies that need to be checked when changing an index.
     /// (15 in the same row, 15 in the same column and then 9 remaining in the box)
@@ -35,65 +23,65 @@ struct ConstantsStorage<Size: SudokuSize> {
     let allIndiciesInColumn: [[Int]]
     let allIndiciesInBox: [[Int]]
     
-    init() {
+    init(size: SudokuSize) {
 
-        self.indiciesAffectedByIndex = Size.allCells.map { index in
+        self.indiciesAffectedByIndex = size.allCells.map { index in
             var indicies = Set<Int>()
-            Self._indiciesInSameRowInclusive(as: index).forEach { indicies.insert($0) }
-            Self._indiciesInSameColumnInclusive(as: index).forEach { indicies.insert($0) }
-            Self._indiciesInSameBoxInclusive(as: index).forEach { indicies.insert($0) }
+            Self._indiciesInSameRowInclusive(as: index, size: size).forEach { indicies.insert($0) }
+            Self._indiciesInSameColumnInclusive(as: index, size: size).forEach { indicies.insert($0) }
+            Self._indiciesInSameBoxInclusive(as: index, size: size).forEach { indicies.insert($0) }
             //Remove self
             indicies.remove(index)
             //TODO: Consider if sorting is needed for deterministic tests here
             return Array(indicies).sorted()
         }
     
-        self.indiciesInSameRowExclusive = Size.allCells.map { index1 in
-            Self._indiciesInSameRowInclusive(as: index1).filter { index2 in index1 != index2 }
+        self.indiciesInSameRowExclusive = size.allCells.map { index1 in
+            Self._indiciesInSameRowInclusive(as: index1, size: size).filter { index2 in index1 != index2 }
         }
         
-        self.indiciesInSameColumnExclusive = Size.allCells.map { index1 in
-            Self._indiciesInSameColumnInclusive(as: index1).filter { index2 in index1 != index2 }
+        self.indiciesInSameColumnExclusive = size.allCells.map { index1 in
+            Self._indiciesInSameColumnInclusive(as: index1, size: size).filter { index2 in index1 != index2 }
         }
         
-        self.indiciesInSameBoxExclusive = Size.allCells.map { index1 in
-            Self._indiciesInSameBoxInclusive(as: index1).filter { index2 in index1 != index2 }
+        self.indiciesInSameBoxExclusive = size.allCells.map { index1 in
+            Self._indiciesInSameBoxInclusive(as: index1, size: size).filter { index2 in index1 != index2 }
         }
             
-        self.allIndiciesInRow = Size.allPossibilities.map { row in
-            Size.allPossibilities.map { offset in row * Size.possibilities + offset }
+        self.allIndiciesInRow = size.allPossibilities.map { row in
+            size.allPossibilities.map { offset in row * size.possibilities + offset }
         }
 
-        self.allIndiciesInColumn = Size.allPossibilities.map { offset in
-            stride(from: 0, to: Size.cells, by: Size.possibilities).map { start in start + offset }
+        self.allIndiciesInColumn = size.allPossibilities.map { offset in
+            stride(from: 0, to: size.cells, by: size.possibilities).map { start in start + offset }
         }
            
-        let starts = Self.boxOffsets.map { $0 * Size.sideOfBox }
+        let starts = Self.boxOffsets(size: size).map { $0 * size.sideOfBox }
         self.allIndiciesInBox = starts.map { start in
-            Self.boxOffsets.map { offset in start + offset }
+            Self.boxOffsets(size: size).map { offset in start + offset }
         }
     }
     
-    private static func _indiciesInSameRowInclusive(as index: Int) -> CountableRange<Int> {
-        let start = (index / Size.possibilities) * Size.possibilities
-        let end = start + Size.possibilities
+    private static func _indiciesInSameRowInclusive(as index: Int, size: SudokuSize) -> CountableRange<Int> {
+        let start = (index / size.possibilities) * size.possibilities
+        let end = start + size.possibilities
         return start..<end
     }
     
-    private static func _indiciesInSameColumnInclusive(as index: Int) -> StrideTo<Int> {
-        stride(from: index % Size.possibilities, to: Size.cells, by: Size.possibilities)
+    private static func _indiciesInSameColumnInclusive(as index: Int, size: SudokuSize) -> StrideTo<Int> {
+        stride(from: index % size.possibilities, to: size.cells, by: size.possibilities)
     }
     
-    private static func _indiciesInSameBoxInclusive(as index: Int) -> [Int] {
-        let row = index / Size.possibilities
-        let column = index % Size.possibilities
-        let startIndexOfBlock = (row / Size.sideOfBox) * Size.possibilities * Size.sideOfBox + (column / Size.sideOfBox) * Size.sideOfBox
-        return Self.boxOffsets.map { startIndexOfBlock + $0 }
+    private static func _indiciesInSameBoxInclusive(as index: Int, size: SudokuSize) -> [Int] {
+        let row = index / size.possibilities
+        let column = index % size.possibilities
+        let startIndexOfBlock = (row / size.sideOfBox) * size.possibilities * size.sideOfBox + (column / size.sideOfBox) * size.sideOfBox
+        return Self.boxOffsets(size: size).map { startIndexOfBlock + $0 }
     }
     
-    private static var boxOffsets: [Int] {
-        stride(from: 0, to: Size.possibilities * Size.sideOfBox, by: Size.possibilities)
-        .flatMap { $0..<($0 + Size.sideOfBox) }
+    private static func boxOffsets(size: SudokuSize) -> [Int] {
+        stride(from: 0, to: size.possibilities * size.sideOfBox, by: size.possibilities)
+        .flatMap { $0..<($0 + size.sideOfBox) }
     }
     
 }
