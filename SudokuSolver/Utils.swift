33770973d4a099c0b474f164ac16551597400045
Michael <michael404@gmail.com>
2020-02-19 17:16:45 +0100

    Removed "public" from WyRands methods

--- SudokuSolver/Utils.swift
+++ SudokuSolver/Utils.swift
@@ -15,11 +15,11 @@ struct WyRand: RNG {
         self.init(seed: rng.next())
     }
 
-    public init(seed: UInt64) {
+    init(seed: UInt64) {
         self.state = seed
     }
 
-    public mutating func next() -> UInt64 {
+    mutating func next() -> UInt64 {
         state &+= 0xa0761d6478bd642f
         let mul = state.multipliedFullWidth(by: state ^ 0xe7037ed1a0b428db)
         return mul.high ^ mul.low
